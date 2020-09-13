643cbadd2645b9ce12ec1dbb208f96e0c0ce266c
Bekir <barslann58@gmail.com>
2020-09-13 16:59:19 -0700

    First commit

---
+++ src/build/precache-manifest.9d00baed9c5346603ecca91c6f61b656.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f946b25d1707512a7c3cb5736feaf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68301d542eb94c1058ef",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "42dcac51a0a1225718b8",
+    "url": "/static/js/2.50dc2fc4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.50dc2fc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68301d542eb94c1058ef",
+    "url": "/static/js/main.1822f791.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
